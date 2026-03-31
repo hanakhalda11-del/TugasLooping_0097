d9039d5160f7867db622883e5ff4f3d712a8e436
hanakhalda11-del <hanakhalda11@gmail.com>
2026-03-31 21:55:03 +0700

    Refactor tampilkanMenu function for improved clarity

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -5,3 +5,12 @@ int angka;
 int pilihan;
 
 void tampilkanMenu() 
+{
+    cout << "\n===== MENU UTAMA =====" << endl;
+    cout << "1. cek bilangan prima" << endl;
+    cout << "2. cek bilangan fibonacci" << endl;
+    cout << "0. keluar" << endl;
+    cout << "pilih menu" << endl;
+    cin >> pilihan;
+}
+
