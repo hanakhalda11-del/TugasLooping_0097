45174c4aaa3783d71c923779455b190963feca47
hanakhalda11-del <hanakhalda11@gmail.com>
2026-03-31 21:56:59 +0700

    Fix formatting issues in tugaslooping.cpp

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -21,3 +21,14 @@ void inputAngka()
 }
 
 bool isPrima(int n)
+{
+    if (n <= 1) return false;
+    int i = 2;
+    while (i * i <= n)
+    {
+        if (n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
+
